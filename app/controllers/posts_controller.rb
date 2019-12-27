3772b1f45a0ae0333c06ec8d6793b74497112c26
arisa <arisankochira10@gmail.com>
2019-12-27 16:00:44 +0900

    画像登録もできるようにする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new #フォーム用の空のインスタンスを引数に
+  end
+  
+  def create
+    @post.save #saveをしてデータベースに保存する。
+    redairect_to @post #showページにリダイレクト
+  end
+
+  private
+
+  def post_params #ストロングパラメータを定義する
+    params.require(:post).permit(:caption)
+  end
 end
